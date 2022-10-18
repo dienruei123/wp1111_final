f3963f49d3ac6694b7fe6c6c02168f6c21779ef6
dienruei123 <dienruei@gmail.com>
2022-10-18 12:26:05 +0800

    Submit

---
+++ hack1/src/components/Board.js
@@ -0,0 +1,28 @@
+/****************************************************************************
+  FileName      [ Board.js ]
+  PackageName   [ src/components ]
+  Author        [ Cheng-Hua Lu ]
+  Synopsis      [ This file generates the Board. ]
+  Copyright     [ 2022 10 ]
+****************************************************************************/
+
+import Row from "./Row";
+import "./css/Board.css";
+import React from "react";
+import CurRow from "./CurRow";
+
+const Board = ({ turn, guesses, curGuess }) => {
+  //   console.log("Board", guesses);
+  return (
+    <div className="Board-container">
+      {/* TODO 2-2: show 6 rows (map function is recommended) and defined row's key.
+                Hint: Use `CurRow` instead of `Row` when you are passing `curGuess` into it. */}
+      {guesses.map((rowGuess, rowIdx) => {
+        if (rowIdx === turn)
+          return <CurRow curGuess={curGuess} rowIdx={rowIdx} key={rowIdx} />;
+        return <Row guess={rowGuess} rowIdx={rowIdx} key={rowIdx} />;
+      })}
+    </div>
+  );
+};
+export default Board;
