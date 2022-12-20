d3e5d09ec549962764c96454595c3d5165a3e45d
dienruei123 <dienruei@gmail.com>
2022-12-20 11:20:31 +0800

    hack3

--- hack3/frontend/src/components/Analytics.js
+++ hack3/frontend/src/components/Analytics.js
@@ -7,11 +7,8 @@ import Category from "./Category"
 
 function Analytics() {
   // TODO 2.2 Use the useQuery hook to get items from backend
-  const {
-    loading,
-    error,
-    data: { items },
-  } = useQuery(GET_ITEMS_QUERY)
+  const { data } = useQuery(GET_ITEMS_QUERY)
+  const items = data.items
   // TODO 2.2 End
 
   return (
