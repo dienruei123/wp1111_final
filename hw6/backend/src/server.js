6ccc79cd10a7eafcc26b4cac3b06d43a08c1d095
dienruei123 <dienruei@gmail.com>
2022-12-26 16:44:57 +0800

    modify backend

--- hw6/backend/src/server.js
+++ hw6/backend/src/server.js
@@ -12,6 +12,11 @@ db.connect()
 // router settings
 const app = express()
 const port = process.env.PORT || 4000
+// Parses the text as JSON and exposes the resulting object on req.body
+app.use(bodyParser.json())
+// app.use(cors())
+app.use("/", routes)
+
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve()
   app.use(express.static(path.join(__dirname, "../frontend", "build")))
@@ -23,11 +28,6 @@ if (process.env.NODE_ENV === "production") {
 if (process.env.NODE_ENV === "development") {
   app.use(cors())
 }
-
-// Parses the text as JSON and exposes the resulting object on req.body
-// app.use(bodyParser.json())
-// app.use(cors())
-// app.use("/", routes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
