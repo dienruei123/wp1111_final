d1798dc01583f097cf9333f973896a8b30628af6
dienruei123 <dienruei@gmail.com>
2022-12-26 16:56:31 +0800

    modify backend

--- hw6/backend/src/server.js
+++ hw6/backend/src/server.js
@@ -15,7 +15,10 @@ const port = process.env.PORT || 4000
 // Parses the text as JSON and exposes the resulting object on req.body
 app.use(bodyParser.json())
 // app.use(cors())
-app.use("/", routes)
+if (process.env.NODE_ENV === "development") {
+  app.use(cors())
+}
+app.use("/api", routes)
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve()
@@ -23,10 +26,6 @@ if (process.env.NODE_ENV === "production") {
   app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"))
   })
-}
-
-if (process.env.NODE_ENV === "development") {
-  app.use(cors())
 }
 
 app.listen(port, () => {
