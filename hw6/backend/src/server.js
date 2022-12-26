06a24b981c4474605efa074a041ddc7aa8cadeea
dienruei123 <dienruei@gmail.com>
2022-12-26 16:26:12 +0800

    upload dockerfile & changes

--- hw6/backend/src/server.js
+++ hw6/backend/src/server.js
@@ -1,21 +1,34 @@
-import express from "express";
-import cors from "cors";
-import db from "./db";
-import bodyParser from "body-parser";
-import routes from "./routes";
+import express from "express"
+import cors from "cors"
+import db from "./db"
+import bodyParser from "body-parser"
+import routes from "./routes"
+
+import path from "path"
 
 // db operations
-db.connect();
+db.connect()
 
 // router settings
-const app = express();
-const port = process.env.PORT || 4000;
+const app = express()
+const port = process.env.PORT || 4000
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve()
+  app.use(express.static(path.join(__dirname, "../frontend", "build")))
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"))
+  })
+}
+
+if (process.env.NODE_ENV === "development") {
+  app.use(cors())
+}
 
 // Parses the text as JSON and exposes the resulting object on req.body
-app.use(bodyParser.json());
-app.use(cors());
-app.use("/", routes);
+// app.use(bodyParser.json())
+// app.use(cors())
+// app.use("/", routes)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
--- hw6/frontend/src/api.js
+++ hw6/frontend/src/api.js
@@ -1,9 +1,13 @@
-import axios from 'axios';
+import axios from "axios"
 
-const instance = axios.create({
-  baseURL: `http://localhost:4000/`,
-});
+const API_ROOT =
+  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:4000/api"
 
-export default instance;
+// const instance = axios.create({
+//   baseURL: `http://localhost:4000/`,
+// });
+
+// export default instance;
+export const api = axios.create({ baseURL: API_ROOT })
 
 // instance.get('/hi').then((data) => console.log(data));
